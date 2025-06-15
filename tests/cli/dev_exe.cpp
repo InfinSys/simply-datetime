b306fa34c0bfbdbb84c3e90274573b4d66730319
Jamon Bailey <infinity.dev@outlook.com>
2025-06-15 17:35:06 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -9,12 +9,18 @@
 *
 \* /// \\\ /// \\\ ///  | END |  \\\ /// \\\ /// \\\ */
 
+#include "simplydt/common/stl_chrono_defs.hpp"
 
 int main(int argc, char* argv[])
 {
     ProjectInfoOut();
 
     //\\//
+    using namespace simplydt;
+
+    stl::SysTimePoint test = std::chrono::system_clock::now();
+
+    std::cout << test << std::endl;
     //
 
     return 0;
