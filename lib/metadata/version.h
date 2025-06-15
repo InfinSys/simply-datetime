9b74140e1e4112c772c5aa6b27cb0a89295dea90
Jamon Bailey <infinity.dev@outlook.com>
2025-06-15 17:47:17 -0400

    Updated Metadata Header Templates
    Includes copyright/license header

--- lib/metadata/version.h
+++ lib/metadata/version.h
@@ -1,4 +1,9 @@
 
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
 /*!
  * @file version.h
  *
