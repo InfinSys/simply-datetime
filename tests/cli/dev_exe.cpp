f499ec689475d19d5068b9e594fd8ee510b039b1
Jamon Bailey <infinity.dev@outlook.com>
2025-06-16 17:59:44 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -5,11 +5,18 @@
 *
 * ~ CLI Debug Executable Notes / TO-DO List ~
 *
-* -> [] n/a
+* -> [] Advocating for strategy-based datetime
+*       implementation so that it is easy to
+*       swap the logic that drives the datetime
+*       type.
+* -> [] Need a general place for stuff like
+        'YEAR_MIN/MAX', 'Invalid' string, etc.
 *
 \* /// \\\ /// \\\ ///  | END |  \\\ /// \\\ /// \\\ */
 
-#include "simplydt/common/stl_chrono_utils.hpp"
+#include "simplydt/coord_universal_time/utc_defs.hpp"
+#include "simplydt/gregorian_calendar/gregorian_defs.hpp"
+#include "simplydt/julian_calendar/julian_defs.hpp"
 
 int main(int argc, char* argv[])
 {
