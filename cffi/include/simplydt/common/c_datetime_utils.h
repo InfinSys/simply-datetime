f0a52d9afdec8e54880c315c95ef0b85f4e2e777
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2025-06-23 16:53:08 -0400

    Create c_datetime_utils.h
    Incomplete

---
+++ cffi/include/simplydt/common/c_datetime_utils.h
@@ -0,0 +1,30 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file c_datetime_utils.h
+ *
+ * @brief
+ * C Standard date and time library utilities.
+ */
+
+
+#ifndef SIMPLYDT_LIB_C_DATE_TIME_UTILITIES_C_H_
+#define SIMPLYDT_LIB_C_DATE_TIME_UTILITIES_C_H_
+
+#include "c_datetime_defs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Start...
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SIMPLYDT_LIB_C_DATE_TIME_UTILITIES_C_H_
