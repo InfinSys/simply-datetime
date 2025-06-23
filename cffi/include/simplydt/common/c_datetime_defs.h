37643d13980d93c746256190489917b9b45b256b
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2025-06-23 16:54:34 -0400

    Codebase Format

--- cffi/include/simplydt/common/c_datetime_defs.h
+++ cffi/include/simplydt/common/c_datetime_defs.h
@@ -15,31 +15,31 @@
 #ifndef SIMPLYDT_LIB_C_DATE_TIME_ALIASES_C_H_
 #define SIMPLYDT_LIB_C_DATE_TIME_ALIASES_C_H_
 
-#include <time.h>
 #include <stdint.h>
+#include <time.h>
 
 /*!
-* @brief
-* Seconds since the Unix epoch.
-*
-* @note
-* This type is the C representation of
-* `simplydt::stl::UnixTimestamp` type.
-*
-* @details
-* Measures seconds elapsed since the Unix
-* epoch (January 1, 1970 00:00:00.000).
-*/
+ * @brief
+ * Seconds since the Unix epoch.
+ *
+ * @note
+ * This type is the C representation of
+ * `simplydt::stl::UnixTimestamp` type.
+ *
+ * @details
+ * Measures seconds elapsed since the Unix
+ * epoch (January 1, 1970 00:00:00.000).
+ */
 typedef int64_t simplydt_UnixTimestamp;
 
 /*!
-* @brief
-* Broken-down calendar component struct.
-*
-* @note
-* This type is the C representation of
-* `simplydt::stl::CalendarDateTime` type.
-*/
+ * @brief
+ * Broken-down calendar component struct.
+ *
+ * @note
+ * This type is the C representation of
+ * `simplydt::stl::CalendarDateTime` type.
+ */
 typedef tm simplydt_CalendarDateTime;
 
 #endif // SIMPLYDT_LIB_C_DATE_TIME_ALIASES_C_H_
