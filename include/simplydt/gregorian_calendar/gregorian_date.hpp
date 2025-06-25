aeb3a9ee63032cf74919f2ef6953bbb12ae339bd
Jamon Bailey <infinity.dev@outlook.com>
2025-06-25 00:25:46 -0400

    Gregorian Date Implementation Setup
    Incomplete

---
+++ include/simplydt/gregorian_calendar/gregorian_date.hpp
@@ -0,0 +1,145 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file gregorian_date.hpp
+ *
+ * @brief
+ * Standalone Gregorian calendar date declaration.
+ */
+
+#ifndef SIMPLYDT_LIB_GREGORIAN_DATE_STRUCT_H_
+#define SIMPLYDT_LIB_GREGORIAN_DATE_STRUCT_H_
+
+#include "simplydt/gregorian_calendar/gregorian_defs.hpp"
+#include "simplydt/common/string_utils.hpp"
+#include <ostream>
+
+namespace simplydt::gregorian
+{
+
+    /*!
+     * @brief
+     * Broken-down Gregorian calendar date.
+     *
+     * @details
+     * This type is meant to serve as a broken-down
+     * form of Gregorian calendar dates and only
+     * serves this purpose. This type does not
+     * validate the Gregorian dates it is constructed
+     * with, it is only a vehicle for Gregorian Date
+     * values.
+     */
+    class Date {
+
+      public:
+        /*!
+         * @brief
+         * Underlying type used to represent Gregorian
+         * calendar dates.
+         */
+        using Underlying_T = uint32_t;
+
+        /*!
+         * @brief
+         * Factor used to store and retrieve year values.
+         */
+        static constexpr Underlying_T YEAR_FACTOR = 10'000;
+
+        /*!
+         * @brief
+         * Factor used to store and retrieve month values.
+         */
+        static constexpr Underlying_T MONTH_FACTOR = 100;
+
+        /*!
+         * @brief
+         * Default Gregorian calendar date.
+         */
+        static constexpr uint32_t DEFAULT_DATE = ((1'970 * YEAR_FACTOR) + 101);
+
+        Date(const YearInt_T year, const uint8_t month, const uint8_t day) noexcept;
+
+        Date(const YearInt_T year, const uint8_t month) noexcept;
+
+        Date(const Date& date) noexcept;
+
+        Date() noexcept;
+
+        ~Date() noexcept = default;
+
+        friend inline std::ostream& operator<<(std::ostream& os, const Date date) noexcept
+        {
+            os << date.toStr();
+            return os;
+        }
+
+        /*!
+         * @brief
+         * Date year component.
+         *
+         * @return
+         * Year of Gregorian calendar date
+         */
+        [[nodiscard]] YearInt_T year() const noexcept;
+
+        /*!
+         * @brief
+         * Date month component.
+         *
+         * @return
+         * Month of Gregorian calendar date
+         */
+        [[nodiscard]] uint8_t month() const noexcept;
+
+        /*!
+         * @brief
+         * Month name.
+         *
+         * @return
+         * Month literal
+         */
+        [[nodiscard]] const char* monthLiteral() const noexcept;
+
+        /*!
+         * @brief
+         * Abbreviated month name.
+         *
+         * @return
+         * Abbreviated month literal
+         */
+        [[nodiscard]] std::string_view monthAbbreviation() const noexcept;
+
+        /*!
+         * @brief
+         * Date day component.
+         *
+         * @return
+         * Day of Gregorian calendar date
+         */
+        [[nodiscard]] uint8_t day() const noexcept;
+
+        /*!
+         * @brief
+         * Compose string representation of Gregorian
+         * calendar date.
+         *
+         * @return
+         * Gregorian calendar date as string
+         */
+        [[nodiscard]] std::string toStr() const noexcept;
+
+      private:
+        Underlying_T date; ///< Calendar date
+
+        void assumeGregorianDate(
+            const YearInt_T& year, const uint8_t& month, const uint8_t& day
+        ) noexcept;
+    };
+
+} // namespace simplydt::gregorian
+
+#endif // SIMPLYDT_LIB_GREGORIAN_DATE_STRUCT_H_
---
+++ src/gregorian_calendar/gregorian_date.cpp
@@ -0,0 +1,112 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+#include "simplydt/gregorian_calendar/gregorian_date.hpp"
+
+//	simplydt::gregorian::Date : CONSTRUCTORS
+
+simplydt::gregorian::Date::Date(
+    const YearInt_T year, const uint8_t month, const uint8_t day
+) noexcept
+    : date{ DEFAULT_DATE }
+{
+    this->assumeGregorianDate(year, month, day);
+}
+
+simplydt::gregorian::Date::Date(const YearInt_T year, const uint8_t month) noexcept
+    : date{ DEFAULT_DATE }
+{
+    this->assumeGregorianDate(year, month, 1);
+}
+
+simplydt::gregorian::Date::Date(const Date& date) noexcept : date{ DEFAULT_DATE }
+{
+    this->date = date.date;
+}
+
+simplydt::gregorian::Date::Date() noexcept : date{ DEFAULT_DATE }
+{
+    //
+}
+
+
+//	simplydt::gregorian::Date : STATIC
+//	simplydt::gregorian::Date : STATIC END!
+
+
+//	simplydt::gregorian::Date : OPERATOR
+//	simplydt::gregorian::Date : OPERATOR END!
+
+
+//	simplydt::gregorian::Date : PUBLIC
+
+simplydt::gregorian::YearInt_T simplydt::gregorian::Date::year() const noexcept
+{
+    return static_cast<YearInt_T>(this->date / YEAR_FACTOR);
+}
+
+uint8_t simplydt::gregorian::Date::month() const noexcept
+{
+    return static_cast<uint8_t>((this->date % YEAR_FACTOR) / MONTH_FACTOR);
+}
+
+const char* simplydt::gregorian::Date::monthLiteral() const noexcept
+{
+    const uint8_t monthIndex = this->month() - 1;
+
+    return Months[monthIndex];
+}
+
+std::string_view simplydt::gregorian::Date::monthAbbreviation() const noexcept
+{
+    const uint8_t monthIndex = this->month() - 1;
+
+    return MonthAbbrevs[monthIndex];
+}
+
+uint8_t simplydt::gregorian::Date::day() const noexcept
+{
+    return static_cast<uint8_t>((this->date % YEAR_FACTOR) % MONTH_FACTOR);
+}
+
+std::string simplydt::gregorian::Date::toStr() const noexcept
+{
+    const char delimiter = '-';
+
+    std::string dateStr;
+    dateStr.reserve(12);
+    dateStr = "";
+
+    dateStr += (std::to_string(this->year()) + delimiter);
+    dateStr += (toDoubleDigitStr(this->month()) + delimiter);
+    dateStr += toDoubleDigitStr(this->day());
+
+    return dateStr;
+}
+
+//	simplydt::gregorian::Date : PUBLIC END!
+
+
+//	simplydt::gregorian::Date : PROTECTED
+//	simplydt::gregorian::Date : PROTECTED END!
+
+
+//	simplydt::gregorian::Date : PRIVATE
+
+void simplydt::gregorian::Date::assumeGregorianDate(
+    const YearInt_T& year, const uint8_t& month, const uint8_t& day
+) noexcept
+{
+    if ((month < MIN_MONTH_OF_YEAR) || (month > MAX_MONTH_OF_YEAR))
+        return; // Disregard params, continue with default date
+
+    if ((day < MIN_DAY_OF_MONTH) || (day > MAX_DAY_OF_MONTH))
+        return; // Disregard params, continue with default date
+
+    this->date = ((year * YEAR_FACTOR) + (month * MONTH_FACTOR) + day);
+}
+
+//	simplydt::gregorian::Date : PRIVATE END!
