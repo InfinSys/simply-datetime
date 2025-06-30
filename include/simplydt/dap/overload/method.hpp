3ef20730d122fd1d7e9576c2d48dff520cad7ef2
Jamon Bailey <infinity.dev@outlook.com>
2025-06-30 06:07:01 -0400

    Create method.hpp
    Explicit type for method overloads in the DAP CRTP-style design

---
+++ include/simplydt/dap/overload/method.hpp
@@ -0,0 +1,37 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file method.hpp
+ *
+ * @brief
+ * Base DAP method overload entity definition.
+ */
+
+#ifndef SIMPLYDT_LIB_CRTP_METHOD_OVERLOAD_STRUCT_H_
+#define SIMPLYDT_LIB_CRTP_METHOD_OVERLOAD_STRUCT_H_
+
+#include "simplydt/dap/overload/arguments.hpp"
+
+namespace simplydt::dap
+{
+
+    /*!
+     * @brief
+     * Base DAP method overload struct.
+     */
+    template <typename... ArgTypes>
+    struct Overload {
+        /*! @brief Overload method argument types. */
+        using MethodArgs = Arguments<ArgTypes...>;
+
+        /*! @brief Conceptual type identification. */
+        static constexpr bool isOverload() noexcept { return true; }
+    };
+
+} // namespace simplydt::dap
+
+#endif // SIMPLYDT_LIB_CRTP_METHOD_OVERLOAD_STRUCT_H_
