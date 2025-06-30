4ceda53adfbd91e10c415fa3820425d57a836e3e
Jamon Bailey <infinity.dev@outlook.com>
2025-06-30 06:17:57 -0400

    Create method_args.hpp
    Type traits for method arguments struct

---
+++ include/simplydt/dap/type_traits/method_args.hpp
@@ -0,0 +1,41 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file method_args.hpp
+ *
+ * @brief
+ * Method arguments entity type traits.
+ */
+
+#ifndef SIMPLYDT_LIB_METHOD_ARGUMENT_TRAITS_H_
+#define SIMPLYDT_LIB_METHOD_ARGUMENT_TRAITS_H_
+
+#include "simplydt/dap/overload/arguments.hpp"
+#include <type_traits>
+
+namespace simplydt::type_trait
+{
+
+    /*! @brief Not valid method arguments type. */
+    template <typename T>
+    struct is_method_args : std::false_type { };
+
+    /*! @brief Valid method arguments type. */
+    template <typename... Args>
+    struct is_method_args<dap::Arguments<Args...>> : std::true_type { };
+
+    /*!
+     * @brief
+     * Determine if type T is a method arguments
+     * struct.
+     */
+    template <typename T>
+    inline constexpr bool is_method_args_v = is_method_args<T>::value;
+
+} // namespace simplydt::type_trait
+
+#endif // SIMPLYDT_LIB_METHOD_ARGUMENT_TRAITS_H_
