d01dce31871b14fd4e413bed45fbc510cb91f3e1
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2025-07-22 23:14:54 -0400

    Create calendar_defs.hpp

---
+++ include/simplydt/common/calendar_defs.hpp
@@ -0,0 +1,45 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file calendar_defs.hpp
+ *
+ * @brief
+ * General calendar-agnostic definitions.
+ */
+
+
+#ifndef SIMPLYDT_LIB_GENERAL_CALENDAR_DEFINITIONS_H_
+#define SIMPLYDT_LIB_GENERAL_CALENDAR_DEFINITIONS_H_
+
+#include "simplydt/common/simplydt_defs.hpp"
+#include <cstdint>
+
+namespace simplydt
+{
+
+/*!
+ * @brief
+ * Enumeration of available calendar systems.
+ */
+enum CalendarSystem : uint8_t {
+    JULIAN,    ///< Proleptic solar calendar
+    GREGORIAN, ///< Civil solar calendar
+};
+
+/*!
+ * @brief
+ * Enumeration of individual calendar components.
+ */
+enum class CalendarComponent : uint8_t {
+    DAY   = DatetimeComponent::DAY,   ///< Calendar day component
+    MONTH = DatetimeComponent::MONTH, ///< Calendar month component
+    YEAR  = DatetimeComponent::YEAR   ///< Calendar year component
+};
+
+} // namespace simplydt
+
+#endif // SIMPLYDT_LIB_GENERAL_CALENDAR_DEFINITIONS_H_
