ad2251eb0c90db75a43ef7873d9f971d1405ddaa
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2025-07-22 23:56:45 -0400

    Create abstract_calendar.hpp
    Incomplete

---
+++ include/simplydt/calendar/abstract_calendar.hpp
@@ -0,0 +1,34 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file abstract_calendar.hpp
+ *
+ * @brief
+ * Base CRTP calendar declaration.
+ */
+
+
+#ifndef SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
+#define SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
+
+#include "simplydt/calendar/concepts/calendar_contract.hpp"
+
+namespace simplydt
+{
+
+/*!
+* @brief
+* Base contract enforcing calendar.
+*/
+template <concepts::contract_abiding_calendar Calendar_Impl>
+struct CalendricalSystem {
+    // Start...
+};
+
+}
+
+#endif // SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
