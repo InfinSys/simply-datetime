75a30d977b35ffeeeaf0421ee3471bd3be1d1f24
Jamon Bailey <infinity.dev@outlook.com>
2025-07-23 00:32:06 -0400

    Create calendar_traits.hpp
    Incomplete

---
+++ include/simplydt/calendar/type_traits/calendar_traits.hpp
@@ -0,0 +1,34 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file calendar_traits.hpp
+ *
+ * @brief
+ * Agnostic calendar type traits.
+ */
+
+
+#ifndef SIMPLYDT_LIB_CALENDAR_TYPE_TRAITS_H_
+#define SIMPLYDT_LIB_CALENDAR_TYPE_TRAITS_H_
+
+#include <type_traits>
+
+/*!
+* @namespace simplydt::type_traits
+* 
+* @brief
+* Simply Datetime library type traits.
+*/
+namespace simplydt::type_traits
+{
+
+// TODO: INCOMPLETE!!!
+// (simplydt::type_traits::calendar???)
+
+}
+
+#endif // SIMPLYDT_LIB_CALENDAR_TYPE_TRAITS_H_
