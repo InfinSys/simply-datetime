18360b920383cb6c161e0ddbcc582e440c1b4ba5
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2025-07-23 00:39:41 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -5,12 +5,7 @@
 *
 * ~ CLI Debug Executable Notes / TO-DO List ~
 *
-* -> [] Advocating for strategy-based datetime
-*       implementation so that it is easy to
-*       swap the logic that drives the datetime
-*       type.
-* -> [] Need a general place for stuff like
-*       'YEAR_MIN/MAX', 'Invalid' string, etc.
+* -> [] N/a
 *
 \* /// \\\ /// \\\ ///  | END |  \\\ /// \\\ /// \\\ */
 
