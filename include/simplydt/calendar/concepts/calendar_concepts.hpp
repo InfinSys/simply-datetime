a0edda80e20417cd3584cf1bbf5bd8b640395c33
Jamon Bailey <infinity.dev@outlook.com>
2025-07-23 00:33:17 -0400

    Create calendar_concepts.hpp

---
+++ include/simplydt/calendar/concepts/calendar_concepts.hpp
@@ -0,0 +1,34 @@
+
+// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file calendar_concepts.hpp
+ *
+ * @brief
+ * Agnostic calendar concepts.
+ */
+
+
+#ifndef SIMPLYDT_LIB_CALENDAR_CONCEPTS_H_
+#define SIMPLYDT_LIB_CALENDAR_CONCEPTS_H_
+
+#include <concepts>
+#include "simplydt/calendar/type_traits/calendar_traits.hpp"
+
+/*!
+* @namespace simplydt::concepts
+* 
+* @brief
+* Simply Datetime library concepts.
+*/
+namespace simplydt::concepts
+{
+
+// TODO: INCOMPLETE!!!
+
+}
+
+#endif // SIMPLYDT_LIB_CALENDAR_CONCEPTS_H_
