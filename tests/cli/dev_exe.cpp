7779e766a7dfb77096621ff27ef124b8dba40d55
Jamon Bailey <infinity.dev@outlook.com>
2025-08-06 01:21:33 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -13,31 +13,32 @@
 #include "simplydt/calendar/abstract_calendar.hpp"
 #include "simplydt/calendar/date/abstract_date.hpp"
 
+// Sample Gregorian date implementation
+struct GregorianDate : public simplydt::CalendarDate<GregorianDate, uint16_t> {
+    using Repr_Type = uint32_t;
+
+    GregorianDate() noexcept
+        : simplydt::CalendarDate<GregorianDate, uint16_t>{ }, date{ 19700101 }
+    {
+        //
+    }
+
+    ~GregorianDate() = default;
+
+    uint16_t year() const noexcept { return 2025; }
+    uint8_t month() const noexcept { return 8; }
+    uint8_t day() noexcept { return 5; }
+
+private:
+    Repr_Type date;
+};
+
+
 int main(int argc, char* argv[])
 {
     ProjectInfoOut();
 
     //\\//
-    // Sample Gregorian date implementation
-    struct GregorianDate : public simplydt::CalendarDate<GregorianDate, uint16_t> {
-        using Repr_Type = uint32_t;
-
-        GregorianDate() noexcept
-            : simplydt::CalendarDate<GregorianDate, uint16_t>{ }, date{ 19700101 }
-        {
-            //
-        }
-
-        ~GregorianDate() = default;
-
-        // Start...
-
-    private:
-        Repr_Type date;
-    };
-
-    //GregorianDate::Date;
-    //GregorianDate::YearInt_t;
     //
 
     return 0;
