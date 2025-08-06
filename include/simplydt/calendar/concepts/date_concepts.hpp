606889facd01a7f45a64d0e796477e76621f70e0
Jamon Bailey <infinity.dev@outlook.com>
2025-08-06 01:21:22 -0400

    Update date_concepts.hpp
    Incomplete

--- include/simplydt/calendar/concepts/date_concepts.hpp
+++ include/simplydt/calendar/concepts/date_concepts.hpp
@@ -32,11 +32,24 @@ namespace simplydt::concepts { }
 * @brief
 * Calendar date concepts.
 */
-namespace simplydt::concepts::date {
+namespace simplydt::concepts::date
+{
 
 // TODO: INCOMPLETE!!!
 // nested_date_types : Check for the expected nested types of a date???
 // concepts::date::has_contextual_nested_types
+
+    template <typename Date_Impl, typename Year_T>
+    concept has_date_component_methods = requires(Date_Impl& d) {
+        //{ d.year() } -> std::same_as<Year_T>;
+        //{ d.month() } -> std::same_as<uint8_t>;
+        { d.day() } -> std::convertible_to<uint8_t>;
+    };
+
+    template <typename Date_Impl>
+    concept has_basic_state_methods = requires(Date_Impl& d) {
+        { d.isDefault() } -> std::same_as<bool>;
+    };
 
 }
 
