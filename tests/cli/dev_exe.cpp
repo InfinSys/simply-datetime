e0d73ee5d07e7e5444a2a2c9990d0e96bd1a04f4
Jamon Bailey <infinity.dev@outlook.com>
2025-08-10 04:28:30 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -13,6 +13,8 @@
 #include "simplydt/calendar/abstract_calendar.hpp"
 #include "simplydt/calendar/date/abstract_date.hpp"
 
+
+
 // Sample Gregorian date interface
 struct GregorianDate : public simplydt::CalendarDate<GregorianDate, uint16_t> {
     /*! @brief Underlying date representation type. */
@@ -33,45 +35,6 @@ struct GregorianDate : public simplydt::CalendarDate<GregorianDate, uint16_t> {
 private:
     Repr_Type date; ///< Calendar date
 };
-
-
-//========================================================\\
-// :: ~ "Managing the Incomplete Type Issue w/ CRTP" ~ :: \\
-//========================================================\\
-//
-// PROBLEM:
-// -> The base CRTP CalendarDate class enforces a concept
-//    called 'contract_abiding_date'.
-// -> The 'contract_abiding_date' concept enforces a set
-//    of sub-concepts that seek to validate aspects of
-//    the derived CRTP classes interface.
-// -> However, the derived classes interface has not yet
-//    been parsed by the compiler. The concept is trying
-//    to validate a type that is incomplete.
-// -> This is because the concept is invoked while we are
-//    actively in the middle of declaring the derived
-//    type and its interface.
-//
-// SOLUTIONS:
-// -> Defer interface validation to elsewhere; but where?
-//      -> (This approach will cause substitution failure
-//          error messages to touch the build log, not
-//          something we want happening...)
-// -> Remove 'contract_abiding_date' concept from CRTP base
-//    and instead enforce a subset of constraints that do
-//    not concern the derived interface. Then throughout the
-//    library, enforce the 'contract_abiding_date' concept
-//    where applicable.
-//
-// NOTES:
-// -> Recall that you made the decision to use CRTP
-//    in a customized manner where the roles of the base
-//    and derived element switched. It is the derived
-//    class that presents the public interface while the
-//    base takes a back seat and just provides common
-//    functionality.
-//
-//========================================================\\
 
 
 
