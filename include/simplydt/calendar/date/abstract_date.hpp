d4f144550931a428033af5ed2b6f7bcb9c3ec425
Jamon Bailey <infinity.dev@outlook.com>
2025-08-10 04:26:46 -0400

    Update abstract_date.hpp

--- include/simplydt/calendar/date/abstract_date.hpp
+++ include/simplydt/calendar/date/abstract_date.hpp
@@ -16,39 +16,63 @@
 #define SIMPLYDT_LIB_BASE_CRTP_CALENDAR_DATE_H_
 
 #include "simplydt/calendar/concepts/date_contract.hpp"
+#include "simplydt/common/calendar_defs.hpp"
+#include <optional>
 
 namespace simplydt
 {
 
 /*!
-* @brief
-* Base contract-enforcing agnostic calendar date.
-* 
-* @details
-* This is Simply Datetime's generic representation
-* of a calendar date. It is not associated with any
-* specific calendar and is not a self-constructable
-* type. It's responsibility is to enforce a set of
-* API requirements on the derived calendar date
-* implementations to ensure compatibility with
-* library internals and consistenecy.
-*/
+ * @brief
+ * Base contract-enforcing agnostic calendar date.
+ *
+ * @details
+ * This is Simply Datetime's generic representation
+ * of a calendar date. It is not associated with any
+ * specific calendar and is not a self-constructable
+ * type. It's responsibility is to enforce a set of
+ * API requirements on the derived calendar date
+ * implementations to ensure compatibility with
+ * library internals and consistenecy.
+ */
+// TODO: Above documentaion needs to updated...
 template <typename Date_Impl, typename Year_T>
-    requires concepts::contract_abiding_date<Date_Impl, Year_T>
 struct CalendarDate {
     /*! @brief Calendar date implementation. */
     using Date = Date_Impl;
     /*! @brief Calendar date year type. */
     using YearInt_t = Year_T;
 
-    // TODO: INCOMPLETE!!!
+    [[nodiscard]] std::optional<YearInt_t> getComponent(const CalendarComponent component) const
+    {
+        switch (component) {
+        case CalendarComponent::YEAR:
+            return thisImpl().year();
+        case CalendarComponent::MONTH:
+            return thisImpl().month();
+        case CalendarComponent::DAY:
+            return thisImpl().day();
+        default:
+            return std::nullopt; // Invalid component request
+        };
+    }
 
-private:
-    CalendarDate() = default;
+  private:
+    CalendarDate()  = default;
     ~CalendarDate() = default;
     friend Date_Impl;
+
+    [[nodiscard]] const Date& thisImpl() const noexcept
+    {
+        return static_cast<const Date&>(*this);
+    }
+
+    [[nodiscard]] Date& thisImpl() noexcept
+    {
+        return static_cast<Date&>(*this);
+    }
 };
 
-}
+} // namespace simplydt
 
 #endif // SIMPLYDT_LIB_BASE_CRTP_CALENDAR_DATE_H_
