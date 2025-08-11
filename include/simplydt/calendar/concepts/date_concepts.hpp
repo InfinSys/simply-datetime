c0a1a55c91f2fda45c58b1888032dbdde6d74cb6
Jamon Bailey <infinity.dev@outlook.com>
2025-08-11 18:45:56 -0400

    Update date_concepts.hpp

--- include/simplydt/calendar/concepts/date_concepts.hpp
+++ include/simplydt/calendar/concepts/date_concepts.hpp
@@ -27,8 +27,7 @@
  * Simply Datetime library concepts.
  */
 namespace simplydt::concepts
-{
-}
+{ }
 
 /*!
  * @namespace simplydt::concepts::date
@@ -41,80 +40,41 @@ namespace simplydt::concepts::date
 
 template <typename Date_Impl, typename Year_T>
 concept has_date_component_methods = requires(const Date_Impl& d) {
-    {
-        d.year()
-    } -> std::same_as<Year_T>;
-
-    {
-        d.month()
-    } -> std::same_as<uint8_t>;
-
-    {
-        d.day()
-    } -> std::same_as<uint8_t>;
+    { d.year() } -> std::same_as<Year_T>;
+    { d.month() } -> std::same_as<uint8_t>;
+    { d.day() } -> std::same_as<uint8_t>;
 };
 
 template <typename Date_Impl>
 concept has_logical_operators = requires(const Date_Impl& d) {
-    {
-        d == d
-    } -> std::same_as<bool>;
-
-    {
-        d < d
-    } -> std::same_as<bool>;
-
-    {
-        d > d
-    } -> std::same_as<bool>;
-
-    {
-        d <= d
-    } -> std::same_as<bool>;
-
-    {
-        d >= d
-    } -> std::same_as<bool>;
+    { d == d } -> std::same_as<bool>;
+    { d < d } -> std::same_as<bool>;
+    { d > d } -> std::same_as<bool>;
+    { d <= d } -> std::same_as<bool>;
+    { d >= d } -> std::same_as<bool>;
 };
 
 template <typename Date_Impl>
 concept has_basic_state_methods = requires(const Date_Impl& d) {
-    {
-        d.isDefault()
-    } -> std::same_as<bool>;
+    { d.isDefault() } -> std::same_as<bool>;
 };
 
 template <typename Date_Impl>
 concept is_stream_out_compatible = requires(std::ostream& os, const Date_Impl& d) {
-    {
-        os << d
-    } -> std::convertible_to<std::ostream&>;
+    { os << d } -> std::convertible_to<std::ostream&>;
 };
 
 template <typename Date_Impl>
 concept has_comparison_methods = requires(const Date_Impl& d) {
-    {
-        d.isBefore(d)
-    } -> std::same_as<bool>;
-
-    {
-        d.isAfter(d)
-    } -> std::same_as<bool>;
+    { d.isBefore(d) } -> std::same_as<bool>;
+    { d.isAfter(d) } -> std::same_as<bool>;
 };
 
 template <typename Date_Impl>
 concept has_date_literal_methods = requires(const Date_Impl& d) {
-    {
-        d.monthLiteral()
-    } -> std::same_as<const char*>;
-
-    {
-        d.monthAbbreviation()
-    } -> std::same_as<std::string>;
-
-    {
-        d.toStr()
-    } -> std::same_as<std::string>;
+    { d.monthLiteral() } -> std::same_as<const char*>;
+    { d.monthAbbreviation() } -> std::same_as<std::string>;
+    { d.toStr() } -> std::same_as<std::string>;
 };
 
 template <typename Date_Impl>
