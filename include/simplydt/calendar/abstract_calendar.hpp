a6c3613681a525f922bb53209923925563d96130
Jamon Bailey <infinity.dev@outlook.com>
2025-08-12 04:24:11 -0400

    Update abstract_calendar.hpp

--- include/simplydt/calendar/abstract_calendar.hpp
+++ include/simplydt/calendar/abstract_calendar.hpp
@@ -12,8 +12,8 @@
  */
 
 
-#ifndef SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
-#define SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
+#ifndef SIMPLYDT_LIB_BASE_CALENDAR_H_
+#define SIMPLYDT_LIB_BASE_CALENDAR_H_
 
 #include "simplydt/calendar/concepts/calendar_contract.hpp"
 
@@ -22,18 +22,29 @@ namespace simplydt
 
 /*!
  * @brief
- * Base contract-enforcing calendar.
+ * Base calendar agnostic system.
  */
-template <concepts::contract_abiding_calendar Calendar_Impl>
+template <typename Calendar_Impl, typename Date_T, typename Month_Enum, typename DOW_Enum>
 struct CalendricalSystem {
-    // TODO: INCOMPLETE!!!
+    /*! @brief Calendar system implementation. */
+    using Calendar = Calendar_Impl;
+    /*! @brief Calendar date. */
+    using Date = Date_T;
+    /*! @brief Year integer type. */
+    using YearInt_t = typename Date::YearInt_t;
+    /*! @brief Enumeration of calendar months. */
+    using Month = Month_Enum;
+    /*! @brief Enumeration of calendar days of week. */
+    using DayOfWeek = DOW_Enum;
+
+    //...
 
   private:
-    CalendricalSystem()  = default;
-    ~CalendricalSystem() = default;
+    CalendricalSystem()  = delete;
+    ~CalendricalSystem() = delete;
     friend Calendar_Impl;
 };
 
 } // namespace simplydt
 
-#endif // SIMPLYDT_LIB_BASE_CRTP_CALENDAR_H_
+#endif // SIMPLYDT_LIB_BASE_CALENDAR_H_
