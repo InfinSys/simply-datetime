42f0723eed03bf6e9c75e23693677bcc308ce5b4
Jamon Bailey <infinity.dev@outlook.com>
2025-08-14 00:50:23 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -19,13 +19,16 @@ int main(int argc, char* argv[])
     //\\//
     using GregorianDate     = simplydt::gregorian::GregorianDate;
     using GregorianCalendar = simplydt::gregorian::GregorianCalendar;
+    using Month = simplydt::gregorian::Month;
 
     constexpr GregorianDate demo{2'025, 8, 13};
     std::cout << "\nDemo date: " << demo << '\n';
 
     constexpr bool isValid      = GregorianCalendar::isValidDate(demo);
     constexpr uint8_t noWeeks   = GregorianCalendar::getWeeksInMonth(demo);
-    constexpr const char* month = GregorianCalendar::getMonthLiteral(demo);
+    constexpr const char* month = GregorianCalendar::getMonthName(demo);
+    constexpr simplydt::gregorian::Month mRepr = GregorianCalendar::getMonthEnumRepr(demo);
+    constexpr const char* dowName = GregorianCalendar::getDayOfWeekName(demo);
     //
 
     std::cout << "\n\n\t[ Complete ]" << std::endl;
