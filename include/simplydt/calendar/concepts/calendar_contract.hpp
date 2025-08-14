9f7f2dd8b5a5a90418572773b8481b793adf0322
Jamon Bailey <infinity.dev@outlook.com>
2025-08-13 21:16:15 -0400

    Update calendar_contract.hpp

--- include/simplydt/calendar/concepts/calendar_contract.hpp
+++ include/simplydt/calendar/concepts/calendar_contract.hpp
@@ -31,6 +31,7 @@ concept contract_abiding_calendar = requires {
     requires calendar::has_contextual_nested_types<Calendar_Impl>;
     requires contract_abiding_date<typename Calendar_Impl::Date>;
     requires calendar::has_characteristic_query_members<Calendar_Impl>;
+    requires calendar::has_calendar_name_arrays<Calendar_Impl>;
     requires calendar::has_calendar_structure_methods<Calendar_Impl>;
     requires calendar::has_date_validation_methods<Calendar_Impl>;
     requires calendar::has_date_conversion_methods<Calendar_Impl>;
