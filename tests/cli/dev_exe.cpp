a10b1b1e71d4135edb1dfa9e7ab024f53b43c530
Jamon Bailey <infinity.dev@outlook.com>
2025-08-14 01:28:57 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -19,16 +19,18 @@ int main(int argc, char* argv[])
     //\\//
     using GregorianDate     = simplydt::gregorian::GregorianDate;
     using GregorianCalendar = simplydt::gregorian::GregorianCalendar;
-    using Month = simplydt::gregorian::Month;
+    using Month             = simplydt::gregorian::Month;
 
-    constexpr GregorianDate demo{2'025, 8, 13};
+    constexpr GregorianDate demo{2'025, 8, 14};
     std::cout << "\nDemo date: " << demo << '\n';
 
-    constexpr bool isValid      = GregorianCalendar::isValidDate(demo);
-    constexpr uint8_t noWeeks   = GregorianCalendar::getWeeksInMonth(demo);
-    constexpr const char* month = GregorianCalendar::getMonthName(demo);
-    constexpr simplydt::gregorian::Month mRepr = GregorianCalendar::getMonthEnumRepr(demo);
-    constexpr const char* dowName = GregorianCalendar::getDayOfWeekName(demo);
+    constexpr bool isValid                         = GregorianCalendar::isValidDate(demo);
+    constexpr uint8_t noWeeks                      = GregorianCalendar::getWeeksInMonth(demo);
+    constexpr const char* month                    = GregorianCalendar::getMonthName(demo);
+    constexpr simplydt::gregorian::Month monthRepr = GregorianCalendar::getMonthEnumRepr(demo);
+    constexpr const char* dowName                  = GregorianCalendar::getDayOfWeekName(demo);
+    constexpr simplydt::gregorian::DayOfWeek dowRepr =
+        GregorianCalendar::getDayOfWeekEnumRepr(demo);
     //
 
     std::cout << "\n\n\t[ Complete ]" << std::endl;
