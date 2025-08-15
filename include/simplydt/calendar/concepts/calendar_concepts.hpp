587deb1d9811a90e9cd5fddd67a825c86e53dccf
Jamon Bailey <infinity.dev@outlook.com>
2025-08-15 00:10:29 -0400

    Legal Notice Banner Update

--- include/simplydt/calendar/concepts/calendar_concepts.hpp
+++ include/simplydt/calendar/concepts/calendar_concepts.hpp
@@ -1,5 +1,5 @@
 
-// Copyright (C) 2023-2025 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Copyright (C) 2026 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
 // Released under the terms of the GNU Affero General Public License version 3.
 
 // [ISJTB-CXX-XL20230401-000001]
