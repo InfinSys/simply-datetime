cfc2d0d0b60d61f13fb8465128c002c21d8199ff
Jamon Bailey <infinity.dev@outlook.com>
2025-08-18 05:00:54 -0400

    Create time_concepts.hpp

---
+++ include/simplydt/time/concepts/time_concepts.hpp
@@ -0,0 +1,33 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and Infinity Systems, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3.
+
+// [ISJTB-CXX-XL20230401-000001]
+
+/*!
+ * @file time_concepts.hpp
+ *
+ * @brief
+ * Agnostic temporal time concepts.
+ */
+
+
+#ifndef SIMPLYDT_LIB_TEMPORAL_TIME_CONCEPTS_H_
+#define SIMPLYDT_LIB_TEMPORAL_TIME_CONCEPTS_H_
+
+//...
+
+/*!
+ * @namespace simplydt::concepts::time
+ *
+ * @brief
+ * Temporal time concepts.
+ */
+namespace simplydt::concepts::time
+{
+
+//...
+
+} // namespace simplydt::concepts::time
+
+#endif // SIMPLYDT_LIB_TEMPORAL_TIME_CONCEPTS_H_
