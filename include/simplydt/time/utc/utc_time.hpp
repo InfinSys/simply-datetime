fdb9d1f521a9cb99751f482995c2a5dca8ae91f0
Jamon Bailey <infinity.dev@outlook.com>
2025-08-18 22:38:39 -0400

    Update utc_time.hpp

--- include/simplydt/time/utc/utc_time.hpp
+++ include/simplydt/time/utc/utc_time.hpp
@@ -17,13 +17,44 @@
 
 #include "simplydt/time/abstract_time.hpp"
 #include "simplydt/time/concepts/time_api_contract.hpp"
-#include "simplydt/time/utc/utc_defs.hpp"
 #include "simplydt/time/units/time_units.hpp"
+#include "simplydt/time/utc/utc_defs.hpp"
 
 namespace simplydt::utc
 {
 
 struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
+    /*!
+     * @brief
+     * Identifies the time system represented by this
+     * implementation.
+     */
+    static constexpr TimeStandard standard = TimeStandard::UTC;
+
+    /*!
+     * @brief
+     * Indicates whether the time system is based on celestial
+     * motion.
+     *
+     * @details
+     * Specifies that UTC is a celestial time system. While its
+     * rate is derived from atomic clocks, it is fundamentally
+     * tied to the Earth's rotation (a celestial phenomenon)
+     * through the mechanism of leap seconds.
+     */
+    static constexpr bool isCelestialTimeSystem = true;
+
+    /*!
+     * @brief
+     * Indicates whether the time system is a pure atomic time
+     * scale.
+     *
+     * @details
+     * Specifies that UTC is not a pure atomic time system which
+     * is a continuous time scale based solely on atomic clocks.
+     */
+    static constexpr bool isPureAtomicTimeSystem = false;
+
     /*! @brief One complete day. */
     static constexpr Time_t FULL_DAY = SECONDS_IN_DAY;
 
@@ -53,10 +84,28 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
         const uint8_t& hour, const uint8_t& minute, const uint8_t& second
     ) noexcept
     {
-        if ((hour >= HOURS_IN_DAY) || (minute >= MINUTES_IN_HOUR) || (second >= SECONDS_IN_MINUTE))
+        if ((hour >= HOURS_IN_DAY) || (minute >= MINUTES_IN_HOUR) ||
+            (second >= SECONDS_IN_MINUTE))
             return MIDNIGHT; // Invalid time parameters
-        
+
         return Base::toSerialSeconds(hour, minute, second);
+    }
+
+    /*!
+     * @brief
+     * Intercepts invalid serial seconds and returns midnight
+     * (00:00:00 AM).
+     *
+     * @return
+     * Midnight if invalid, provided time-in-day value otherwise
+     */
+    [[nodiscard]] static constexpr Time_t useDefaultIfInvalid(const Seconds& secs_in_day
+    ) noexcept
+    {
+        if ((secs_in_day.count() < MIDNIGHT) || (secs_in_day.count() >= SECONDS_IN_DAY))
+            return MIDNIGHT;
+
+        return static_cast<Time_t>(secs_in_day.count());
     }
 
     /*!
@@ -65,8 +114,7 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
      * values.
      */
     constexpr UTCTime(const uint8_t hour, const uint8_t minute, const uint8_t second) noexcept
-        : TimeSystem<UTCTime, Time_t>{},
-        timeInDay{useDefaultIfInvalid(hour, minute, second)}
+        : TimeSystem<UTCTime, Time_t>{}, timeInDay{useDefaultIfInvalid(hour, minute, second)}
     {
         //
     }
@@ -77,8 +125,7 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
      * assume second = 0.
      */
     constexpr UTCTime(const uint8_t hour, const uint8_t minute) noexcept
-        : TimeSystem<UTCTime, Time_t>{},
-        timeInDay{useDefaultIfInvalid(hour, minute, 0)}
+        : TimeSystem<UTCTime, Time_t>{}, timeInDay{useDefaultIfInvalid(hour, minute, 0)}
     {
         //
     }
@@ -89,8 +136,7 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
      * minute and second = 0.
      */
     explicit constexpr UTCTime(const uint8_t hour) noexcept
-        : TimeSystem<UTCTime, Time_t>{},
-        timeInDay{useDefaultIfInvalid(hour, 0, 0)}
+        : TimeSystem<UTCTime, Time_t>{}, timeInDay{useDefaultIfInvalid(hour, 0, 0)}
     {
         //
     }
@@ -101,8 +147,7 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
      * day.
      */
     constexpr UTCTime(const Seconds secs_in_day) noexcept
-        : TimeSystem<UTCTime, Time_t>{},
-        timeInDay{useDefaultIfInvalid(secs_in_day.count())}
+        : TimeSystem<UTCTime, Time_t>{}, timeInDay{useDefaultIfInvalid(secs_in_day)}
     {
         //
     }
@@ -124,16 +169,127 @@ struct UTCTime final : public TimeSystem<UTCTime, Time_t> {
         return os;
     }
 
+    /*! @brief Evaluate equivalence of UTC times. */
+    [[nodiscard]] constexpr bool operator==(const UTCTime time) const noexcept
+    {
+        return this->timeInDay == time.timeInDay;
+    }
+
+    /*! @brief Determine if left-hand side is sequentially before right-hand side. */
+    [[nodiscard]] constexpr bool operator<(const UTCTime time) const noexcept
+    {
+        return this->timeInDay < time.timeInDay;
+    }
+
+    /*! @brief Determine if left-hand side is sequentially after right-hand side. */
+    [[nodiscard]] constexpr bool operator>(const UTCTime time) const noexcept
+    {
+        return this->timeInDay > time.timeInDay;
+    }
+
+    [[nodiscard]] constexpr bool operator<=(const UTCTime time) const noexcept
+    {
+        return this->timeInDay <= time.timeInDay;
+    }
+
+    [[nodiscard]] constexpr bool operator>=(const UTCTime time) const noexcept
+    {
+        return this->timeInDay >= time.timeInDay;
+    }
+
+    /*! @brief Index UTC time components. */
+    [[nodiscard]] std::optional<uint8_t> operator[](const TimeComponent component
+    ) const noexcept
+    {
+        // TODO: INCOMPLETE!!!
+        return std::nullopt;
+    }
+
+    /*!
+     * @brief
+     * Evaluate if time is midnight.
+     *
+     * @return
+     * True if time is midnight
+     */
+    [[nodiscard]] constexpr bool isDefault() const noexcept
+    {
+        return this->timeInDay == MIDNIGHT;
+    }
+
+    /*!
+     * @brief
+     * Time hour component (24-hour).
+     *
+     * @return
+     * Hour of UTC time
+     */
     [[nodiscard]] constexpr uint8_t hour() const noexcept
     {
         return Base::hourFromSerialSecs(this->timeInDay);
     }
 
+    /*!
+     * @brief
+     * Time hour component (12-hour).
+     *
+     * @return
+     * Hour of UTC time
+     */
+    [[nodiscard]] constexpr uint8_t hour12() const noexcept
+    {
+        return Base::hourFromSerialSecs(this->timeInDay) % 12;
+    }
+
+    /*!
+     * @brief
+     * Time meridiem phase literal.
+     *
+     * @return
+     * Meridiem phase abbreviation
+     */
+    [[nodiscard]] constexpr const char* hourPhaseStr() const noexcept
+    {
+        if (this->hour() >= 12)
+            return MeridiemPhases[PM];
+
+        return MeridiemPhases[AM];
+    }
+
+    /*!
+     * @brief
+     * Time meridiem phase indicator.
+     *
+     * @return
+     * Meridiem phase enum representation
+     */
+    [[nodiscard]] constexpr MeridiemPhase hourPhaseEnumRepr() const noexcept
+    {
+        if (this->hour() >= 12)
+            return MeridiemPhase::PM;
+
+        return MeridiemPhase::AM;
+    }
+
+    /*!
+     * @brief
+     * Time minute component.
+     *
+     * @return
+     * Minute of UTC time
+     */
     [[nodiscard]] constexpr uint8_t minute() const noexcept
     {
         return Base::minuteFromSerialSecs(this->timeInDay);
     }
 
+    /*!
+     * @brief
+     * Time second component.
+     *
+     * @return
+     * Second of UTC time
+     */
     [[nodiscard]] constexpr uint8_t second() const noexcept
     {
         return Base::secondFromSerialSecs(this->timeInDay);
