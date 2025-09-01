055d4b6d1f00333e1459264672be9b512f978d6d
Jamon Bailey <infinity.dev@outlook.com>
2025-09-01 04:00:30 -0400

    Update gregorian_date.hpp

--- include/simplydt/calendar/gregorian/gregorian_date.hpp
+++ include/simplydt/calendar/gregorian/gregorian_date.hpp
@@ -19,6 +19,7 @@
 #include "simplydt/calendar/gregorian/date_validation.hpp"
 #include "simplydt/calendar/gregorian/gregorian_defs.hpp"
 #include "simplydt/calendar/gregorian/helper_algorithms.hpp"
+#include "simplydt/common/string_utils.hpp"
 
 namespace simplydt::gregorian
 {
@@ -82,7 +83,89 @@ struct GregorianDate :
 
     ~GregorianDate() = default;
 
-    // Continue...
+    /*!
+     * @brief
+     * Date year component.
+     * 
+     * @details
+     * TODO: INCOMPLETE COMMENT!!!
+     *
+     * @return
+     * Year of Gregorian calendar date
+     */
+    [[nodiscard]] constexpr YearInt_t year() const noexcept
+    {
+        // CREDITS: Howard Hinnant [Mr. Chrono] - (Ripple Labs)
+        Repr_Type serial_days = this->serialDays + 719'468;
+        const int era      = (serial_days >= 0 ? serial_days : serial_days - 146'096) / 146'097;
+        const unsigned doe = static_cast<unsigned>(serial_days - era * 146'097);
+        const unsigned yoe = (doe - doe / 1'460 + doe / 36'524 - doe / 146'096) / DAYS_IN_YEAR;
+        return static_cast<YearInt_t>(yoe) + era * YEARS_IN_ERA;
+    }
+
+    /*!
+     * @brief
+     * Date month component.
+     * 
+     * @details
+     * TODO: INCOMPLETE COMMENT!!!
+     *
+     * @return
+     * Month of Gregorian calendar date
+     */
+    [[nodiscard]] constexpr uint8_t month() const noexcept
+    {
+        // CREDITS: Howard Hinnant [Mr. Chrono] - (Ripple Labs)
+        Repr_Type serial_days = this->serialDays + 719'468;
+        const int era      = (serial_days >= 0 ? serial_days : serial_days - 146'096) / 146'097;
+        const unsigned doe = static_cast<unsigned>(serial_days - era * 146'097);
+        const unsigned yoe = (doe - doe / 1'460 + doe / 36'524 - doe / 146'096) / DAYS_IN_YEAR;
+        const unsigned doy = doe - (DAYS_IN_YEAR * yoe + yoe / 4 - yoe / 100);
+        const unsigned mp  = (5 * doy + 2) / 153;
+        return static_cast<uint8_t>(mp + (mp < 10 ? 3 : -9));
+    }
+
+    /*!
+     * @brief
+     * Date day component.
+     * 
+     * @details
+     * TODO: INCOMPLETE COMMENT!!!
+     *
+     * @return
+     * Day of Gregorian calendar date
+     */
+    [[nodiscard]] constexpr uint8_t day() const noexcept
+    {
+        // CREDITS: Howard Hinnant [Mr. Chrono] - (Ripple Labs)
+        Repr_Type serial_days = this->serialDays + 719'468;
+        const int era      = (serial_days >= 0 ? serial_days : serial_days - 146'096) / 146'097;
+        const unsigned doe = static_cast<unsigned>(serial_days - era * 146'097);
+        const unsigned yoe = (doe - doe / 1'460 + doe / 36'524 - doe / 146'096) / DAYS_IN_YEAR;
+        const unsigned doy = doe - (DAYS_IN_YEAR * yoe + yoe / 4 - yoe / 100);
+        const unsigned mp  = (5 * doy + 2) / 153;
+        return static_cast<uint8_t>(doy - (153 * mp + 2) / 5 + 1);
+    }
+
+    /*!
+     * @brief
+     * Compose string representation of Gregorian
+     * calendar date.
+     *
+     * @return
+     * Gregorian calendar date as string
+     */
+    [[nodiscard]] std::string toStr() const noexcept
+    {
+        const char delimiter = '-';
+
+        std::string dateStr;
+        dateStr.reserve(12);
+        dateStr += (std::to_string(this->year()) + delimiter);
+        dateStr += (toDoubleDigitStr(this->month()) + delimiter);
+        dateStr += toDoubleDigitStr(this->day());
+        return dateStr;
+    }
 };
 
 } // namespace simplydt::gregorian
